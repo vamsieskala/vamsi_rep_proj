c166c70d09f97bb3d84c693a0a08e87a39ed1854
vamsieskala <eskalaevk8@gmail.com>
2023-03-03 19:44:51 -0800

    added new file

--- reading_csv/reading_csv_using_pandas.py
+++ reading_csv/reading_csv_using_pandas.py
@@ -1,3 +1,5 @@
 import pandas as pd
-df = pd.read_csv("C:\Users\indian\Desktop\python_csv_files")
-df.sow()
+df = pd.read_csv('C:\Users\indian\Desktop\python_csv_files\details.csv')
+df.tostring()
+
+
